e87bcd6b4b97016542ca1d5d86c9621b58253a23
sarthak <sarthakx67@gmail.com>
2025-08-04 23:10:38 +0530

    Shell-Script

--- 08-catalogue.sh
+++ 08-catalogue.sh
@@ -94,7 +94,7 @@ VALIDATE $? "Starting Catalogue"
 
 touch /etc/yum.repos.d/mongo.repo
 
-cp /RoboShop-Shell-Script-For-Alma-Linux/01-mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE   #
+cp /root/RoboShop-Shell-Script-For-Alma-Linux/01-mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE   #
 
 VALIDATE $? "Copying mongo repo"
 
